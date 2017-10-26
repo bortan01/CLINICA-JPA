dc2d4057d484b3e068ad6c466dd20191bbf57fe0
bortan01 <brmiranda009@gmail.com>
2017-10-26 09:46:14 -0600

    YA SIRVE

--- Clinica/src/java/controlador/beans/DoctorBean.java
+++ Clinica/src/java/controlador/beans/DoctorBean.java
@@ -1,69 +1,74 @@
 package controlador.beans;
 
 import java.io.Serializable;
-import javax.faces.bean.ManagedBean;
-
-import javax.faces.bean.ViewScoped;
-import modelo.entidades.Doctor;
-import session.beans.DoctorFacade;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+import javax.inject.Inject;
+import modelo.entidades.Doctor;
+import session.beans.DoctorFacade;
 
 
-@ManagedBean (name= "doctorBean")
+@ManagedBean (name ="doctorBean")
 @ViewScoped
 public class DoctorBean implements Serializable{
-DoctorFacade DoctorFac;
-Doctor DoctorSeleccionado;    
+@Inject
+DoctorFacade doctorFac;
+Doctor doctorSeleccionado ;    
 List<Doctor> listaDoctor;
 List<Doctor> DoctorFiltrado;
+private SelectItem[] opcionesGenero = new SelectItem[]{new SelectItem("MASCULINO"),
+        new SelectItem("FEMENINO")};
+    private String opcionActualGenero;
 
-    @PostConstruct
+ @PostConstruct
     public void init() {
-        DoctorSeleccionado = new Doctor();
-        BuscarDoctor();
+        doctorSeleccionado = new Doctor();
+        BuscarClinicas();
     }
-    
+
     public DoctorBean() {
     }
     
-     public List<Doctor>BuscarDoctor(){
-    listaDoctor = DoctorFac.findAll();
+    public List<Doctor>BuscarClinicas(){
+    listaDoctor = doctorFac.findAll();
     return listaDoctor;
     }
     
-public String eliminarDcotor() {
-        DoctorFac.remove(DoctorSeleccionado);
+public String eliminarClinica() {
+        doctorFac.remove(doctorSeleccionado);
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "informacion", "Datos Eliminados"));
         return "listar_doctor.xhtml?faces-redirect=true";
     }
     
-    public String actualizarDoctor() {
-        DoctorFac.edit(DoctorSeleccionado);
+    public String actualizarClinica() {
+        doctorFac.edit(doctorSeleccionado);
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "informacion", "Datos Modificados"));
         return "listar_doctor.xhtml?faces-redirect=true";
     }
-  public String insertarDoctor(){
-        DoctorFac.create(DoctorSeleccionado);
+  public String insertarClinica() {
+        doctorFac.create(doctorSeleccionado);
             return "listar_doctor.xhtml?faces-redirect=true";
     }
 
     public DoctorFacade getDoctorFac() {
-        return DoctorFac;
+        return doctorFac;
     }
 
-    public void setDoctorFac(DoctorFacade DoctorFac) {
-        this.DoctorFac = DoctorFac;
+    public void setDoctorFac(DoctorFacade doctorFac) {
+        this.doctorFac = doctorFac;
     }
 
     public Doctor getDoctorSeleccionado() {
-        return DoctorSeleccionado;
+        return doctorSeleccionado;
     }
 
-    public void setDoctorSeleccionado(Doctor DoctorSeleccionado) {
-        this.DoctorSeleccionado = DoctorSeleccionado;
+    public void setDoctorSeleccionado(Doctor doctorSeleccionado) {
+        this.doctorSeleccionado = doctorSeleccionado;
     }
 
     public List<Doctor> getListaDoctor() {
@@ -81,6 +86,22 @@ public String eliminarDcotor() {
     public void setDoctorFiltrado(List<Doctor> DoctorFiltrado) {
         this.DoctorFiltrado = DoctorFiltrado;
     }
-    
-    
+
+    public SelectItem[] getOpcionesGenero() {
+        return opcionesGenero;
+    }
+
+    public void setOpcionesGenero(SelectItem[] opcionesGenero) {
+        this.opcionesGenero = opcionesGenero;
+    }
+
+    public String getOpcionActualGenero() {
+        return opcionActualGenero;
+    }
+
+    public void setOpcionActualGenero(String opcionActualGenero) {
+        this.opcionActualGenero = opcionActualGenero;
+    }
+
+
 }
